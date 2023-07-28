017288ba4c24605e3ce45b044aa8c804bb1cf1a2
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-07-27 23:44:48 -0500

    changed some more things with login/signup/logout.js

--- public/signup.js
+++ public/signup.js
@@ -1,23 +1,26 @@
-const signUpForm = async (event) => {
+const signupFormHandler = async (event) => {
     event.preventDefault();
-
-    const username = document.querySelector('#username').value.trim();
+  
+    const name = document.querySelector('#name').value.trim();
+    const email = document.querySelector('#email').value.trim();
     const password = document.querySelector('#password').value.trim();
+    
 
-    if (username && password) {
-        const response = await fetch("/signup", {
+    if (name && email && password ) {
+      const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ name, email, password}),
         headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
     }
-};
+  };
 
 document
     .querySelector('#signup-form')
-    .addEventListener('submit', signUpForm);
+    .addEventListener('submit', signupFormHandler);
