9131d3da3c3e7ca26a2aee445bcc3e1da5a4db02
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-07-27 23:17:40 -0500

    changing the routes and routing structure

--- models/index.js
+++ models/index.js
@@ -11,4 +11,24 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
     });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+    });
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+    });
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+  });
+
+  
+
 module.exports = { User, Comment, Post };
